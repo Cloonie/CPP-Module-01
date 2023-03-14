7b9a1d3332a8c9f662dcd4b3944e5e14993c86bb
Mark Liew <mliew@u90z03s01.42kl.edu.my>
2023-03-14 21:41:44 +0800

    .

---
+++ ex05/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 19:42:27 by mliew             #+#    #+#             */
+/*   Updated: 2023/03/14 21:21:40 by mliew            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+
+	public:
+		Harl();
+		~Harl();
+		typedef void (Harl::*PtrToMember)(std::string&);
+		void complain(std::string level);
+};
+
+#endif
